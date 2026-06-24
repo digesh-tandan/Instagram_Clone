ded5a478d3644e5a8a8c1378c54de5c59efba6d3
Digesh Tandan <digeshtandan9212@gmail.com>
2026-06-24 12:55:07 +0530

    Updated CORS for Vercel

--- instagram-backend/server.js
+++ instagram-backend/server.js
@@ -96,9 +96,10 @@ app.use(express.urlencoded({
 
 app.use(cors({
 
-    origin:
-    "http://localhost:5173",
-
+    origin: [
+      "http://localhost:5173",
+      "https://YOUR-VERCEL-DOMAIN.vercel.app"
+    ],
     credentials: true
 }));
 
