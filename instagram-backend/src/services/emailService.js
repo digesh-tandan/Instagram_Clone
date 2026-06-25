87367f853cafacdb10deb39ba1c1a5abe9b61c64
Digesh Tandan <digeshtandan9212@gmail.com>
2026-06-25 13:01:18 +0530

    Update Nodemailer

--- instagram-backend/src/services/emailService.js
+++ instagram-backend/src/services/emailService.js
@@ -1,21 +1,22 @@
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
+
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 587,
     secure: false,
-    requireTLS: true,
-
-    family: 4,
 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
 
-    tls: {
-        rejectUnauthorized: false
-    }
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
 });
 
 transporter.verify(function (error, success) {
